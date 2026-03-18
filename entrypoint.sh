fc17c8c8c5c9776caa1a38ee533122cae1056e0a
Ivan <vanyasirenko@gmail.com>
2026-03-18 21:13:28 +0200

    fix entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 composer install --no-interaction --prefer-dist --optimize-autoloader
 
 mkdir -p runtime web/assets
-chown -R www-data:www-data /var/www/html
+chown -R www-data:www-data runtime web/assets vendor
 chmod -R 755 runtime web/assets
 
 exec apache2-foreground
